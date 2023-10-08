8a5846d3757924fd7fdfa7010a74a85571ccb9f0
Semih <85266757+PSEMO@users.noreply.github.com>
2023-10-08 12:56:29 +0300

    update on timings

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ time.sleep(2)
 #--------------------------------------------
 
 def click(x,y):
-    time.sleep(0.01)
+    time.sleep(0.005)
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-    time.sleep(0.01)
+    time.sleep(0.005)
 
 #--------------------------------------------
 
@@ -31,7 +31,7 @@ while 1:
         click(cookiePos[0], cookiePos[1])
         clickCount += 1
 
-        if clickCount > 1500:#Works every once in a while.
+        if clickCount > 2000:#Works every once in a while.
             clickCount = 0#Resets the "timer"
 
             goldenCookie = pyautogui.locateOnScreen('cookie.png')#Finds if golden one exists
